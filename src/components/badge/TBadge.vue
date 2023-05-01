f38e7ae3e471d6e7b3ff8720e54695ad287ca7df
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2023-05-02 01:09:39 +0300

    adds TBadge component

---
+++ src/components/badge/TBadge.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+const props = defineProps({
+  radius: {
+    type: String,
+    default: 'md'
+  },
+  size: {
+    type: String,
+    default: 'sm'
+  },
+  color: {
+    type: String,
+    default: 'blue'
+  },
+  style: {
+    type: String,
+    default: 'solid'
+  }
+})
+</script>
+
+<template>
+  <span class="badge" :class="[
+      `radius-${radius}`,
+      `${style}-${color}`,
+      `size-${size}`,
+  ]">
+    <slot></slot>
+  </span>
+</template>
+```

89873c2c03bca5a10e276a5503be2557eebaba7a
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2023-05-01 04:43:08 +0300

    adds user info from auth store

--- src/layouts/partials/UserMenu.vue
+++ src/layouts/partials/UserMenu.vue
@@ -1,14 +1,21 @@
-<script>
-import {defineComponent} from "vue";
+<script setup lang="ts">
+import {useAuthStore} from "@/stores/auth";
 import TAvatar from "@/components/avatar/TAvatar.vue";
 
-export default defineComponent({
-  components: {TAvatar}
+defineProps({
+  title: String,
+  subtitle: String,
 })
+
+const authStore = useAuthStore()
 </script>
 
 <template>
-  <div>
+  <div v-if="authStore.user" class="flex items-center gap-4">
     <t-avatar/>
+    <div class="hidden xl:flex flex-col">
+      <span class="text-sm" v-text="authStore.user.name"></span>
+      <span class="text-xs font-thin text-slate-500" v-text="authStore.user.title"></span>
+    </div>
   </div>
 </template>

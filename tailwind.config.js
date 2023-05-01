d583be41b77011c9be3fe24f8c1d63d2a419abd0
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2023-05-02 01:10:33 +0300

    chnges font family to Quicksand

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,vue}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx,vue}",
+    ],
+    theme: {
+        extend: {
+        },
+        fontFamily: {
+            sans: [
+                '"Quicksand"',
+                'sans-serif',
+                ...defaultTheme.fontFamily.sans
+            ],
+        }
+    },
+    plugins: [],
 }
 

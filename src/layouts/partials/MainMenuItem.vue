9f94aab55b57ce6a5d8fd0dfea32e4942ea12db0
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2023-05-02 20:56:58 +0300

    parses main menu areas to sub components

--- src/layouts/partials/MainMenuItem.vue
+++ src/layouts/partials/MainMenuItem.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import {IconChevronDown} from "@tabler/icons-vue";
-import {ref} from "vue";
-import {onClickOutside} from '@vueuse/core'
-import TBadge from "@/components/badge/TBadge.vue";
+import { IconChevronDown } from '@tabler/icons-vue'
+import { ref } from 'vue'
+import { onClickOutside } from '@vueuse/core'
+import MainMenuSubItems from "@/layouts/partials/MainMenuSubItems.vue";
 
 defineProps({
   link: {
@@ -13,21 +13,27 @@ defineProps({
 const refSubMenu = ref(null)
 
 const showMenu = ref(false)
-const toggleMenu = () => showMenu.value = !showMenu.value
-onClickOutside(refSubMenu, (event) => showMenu.value = false)
+const toggleMenu = () => (showMenu.value = !showMenu.value)
+onClickOutside(refSubMenu, (event) => (showMenu.value = false))
 </script>
 
 <template>
   <div class="relative" ref="refSubMenu">
     <div class="flex items-center px-3 py-2 gap-2 cursor-pointer" @click="toggleMenu">
       <!--Icon-->
-      <component v-if="link.icon" :is="link.icon" :size="20" :stroke-width="1" class="hidden lg:block"/>
+      <component
+        v-if="link.icon"
+        :is="link.icon"
+        :size="20"
+        :stroke-width="1"
+        class="hidden lg:block"
+      />
 
       <!--Label-->
       <span v-text="link.label" class="text-sm"></span>
 
       <!--Dropdown-->
-      <IconChevronDown v-if="link.type === 'dropdown'" :size="12" class="-ml-1 mt-1"/>
+      <IconChevronDown v-if="link.type === 'dropdown'" :size="12" class="-ml-1 mt-1" />
     </div>
 
     <!--Sub Links-->
@@ -35,21 +41,11 @@ onClickOutside(refSubMenu, (event) => showMenu.value = false)
       <ul class="flex">
         <li v-for="column in link.children" class="flex min-w-[11rem]">
           <ul class="w-full">
-            <li v-for="subLink in column"
-                class="flex justify-between items-center font-semibold hover:bg-slate-100 px-3 py-2 text-sm cursor-pointer whitespace-nowrap select-none">
-              <!--Label-->
-              <span v-text="subLink.label"></span>
-
-              <!--Badge-->
-              <t-badge
-                  v-if="subLink.badge"
-                  :color="subLink.badge?.color"
-                  :style="subLink.badge?.style"
-                  :radius="subLink.badge?.radius"
-              >{{subLink.badge.label}}</t-badge>
-
-              <!--Dropdown-->
-              <IconChevronDown v-if="subLink.type === 'dropdown'" :size="12" class="mt-1 -rotate-90"/>
+            <li
+              v-for="subLink in column"
+              class="flex justify-between items-center font-semibold hover:bg-slate-100 px-3 py-2 text-sm cursor-pointer whitespace-nowrap select-none"
+            >
+              <main-menu-sub-items :link="subLink" />
             </li>
           </ul>
         </li>
---
+++ src/layouts/partials/MainMenuSubItems.vue
@@ -0,0 +1,43 @@
+<script setup>
+import TBadge from "@/components/badge/TBadge.vue";
+
+defineProps({
+  link: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="flex items-center gap-2">
+    <!--Icon-->
+    <component
+        v-if="link.icon"
+        :is="link.icon"
+        :class="link?.style?.icon"
+        :size="16"
+        :stroke-width="1"
+        class="hidden lg:block"
+    />
+
+    <!--Label-->
+    <span v-text="link.label" :class="link?.style?.label"></span>
+  </div>
+
+  <!--Badge-->
+  <t-badge
+      v-if="link.badge"
+      :color="link.badge?.color"
+      :style="link.badge?.style"
+      :radius="link.badge?.radius"
+  >{{ link.badge.label }}
+  </t-badge>
+
+  <!--Dropdown-->
+  <IconChevronDown
+      v-if="link.type === 'dropdown'"
+      :size="12"
+      class="mt-1 -rotate-90"
+  />
+</template>

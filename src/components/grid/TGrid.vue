fe723b98e53d412c5c7e78c1140f1004caf454e4
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2023-05-03 00:37:59 +0300

    adds TGrid component

---
+++ src/components/grid/TGrid.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+defineProps({
+  sm: {
+    type: Number,
+    default: 1
+  },
+  md: {
+    type: Number,
+    default: 2
+  },
+  lg: {
+    type: Number,
+    default: 3
+  },
+  xl: {
+    type: Number,
+    default: 4
+  },
+  xxl: {
+    type: Number,
+    default: 6
+  },
+  gap: {
+    type: Number,
+    default: 4
+  }
+})
+</script>
+
+<template>
+  <div
+      class="grid w-full"
+      :class="[
+      `xxl-grid-cols-${xxl}`,
+      `xl-grid-cols-${xl}`,
+      `lg-grid-cols-${lg}`,
+      `md-grid-cols-${md}`,
+      `sm-grid-cols-${sm}`,
+      `grid-gap-${gap}`
+  ]"
+  >
+    <slot/>
+  </div>
+</template>

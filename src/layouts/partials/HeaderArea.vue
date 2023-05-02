5020aa1af60ac47cec30998b9b5f98c61b6eee84
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2023-05-03 00:40:59 +0300

    fixes logo size

--- src/layouts/partials/HeaderArea.vue
+++ src/layouts/partials/HeaderArea.vue
@@ -4,12 +4,12 @@ import NotificationPoint from '@/layouts/partials/NotificationPoint.vue'
 import UserMenu from '@/layouts/partials/UserMenu.vue'
 </script>
 <template>
-  <div class="flex h-14 py-1 border-b">
-    <div class="flex flex-grow lg:max-w-7xl px-2.5 mx-auto justify-between items-center">
+  <div class="flex flex-shrink-0 h-14 py-1 border-b bg-white">
+    <div class="t-container justify-between items-center">
       <!--Logo-->
-      <div class="flex items-center gap-2 font-bold text-xl">
-        <img alt="logo" src="@/assets/logo.png" class="h-8 w-8 rounded" />
-        ta tabler
+      <div class="flex items-center gap-2 font-bold text-2xl">
+        <img alt="logo" src="@/assets/logo.png" class="h-8 w-8 rounded mr-2" />
+        <span>ta tabler</span>
       </div>
       <!--Left Slot-->
       <slot name="left"></slot>

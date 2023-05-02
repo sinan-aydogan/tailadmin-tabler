dbb0770bb11f514ca170bd91f374585ab6cc7524
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2023-05-03 00:38:55 +0300

    adds left&right slots and upgrades design

--- src/layouts/partials/FooterArea.vue
+++ src/layouts/partials/FooterArea.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>Footer</div>
+  <div class="py-8 mt-[4.50rem]">
+    <div class="t-container">
+      <div class="flex flex-col-reverse lg:flex-row flex-grow justify-between items-center">
+        <!--Left area-->
+        <slot name="left" />
+
+        <!--Right area-->
+        <slot name="right" />
+      </div>
+    </div>
+  </div>
 </template>
